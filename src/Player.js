1865074939b99a767e5ff5f24603bd411bb4e477
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-04 15:41:14 +0100

    handle jumps

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,38 @@
-import {RigidBody} from "@react-three/rapier";
+import {RigidBody, useRapier} from "@react-three/rapier";
 import {useFrame} from "@react-three/fiber";
 import {useKeyboardControls} from "@react-three/drei";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 export default function Player() {
     const body = useRef();
     const [subscribeKeys, getKeys] = useKeyboardControls();
+    const {rapier, world} = useRapier();
+
+    const jump = () => {
+        const origin = body.current.translation();
+        origin.y -= 0.31;
+        const direction = {x: 0, y: -1, z: 0};
+        const ray = new rapier.Ray(origin, direction);
+        const hit = world.castRay(ray, 10, true);
+
+        if(hit.timeOfImpact < 0.15) {
+            body.current.applyImpulse({x: 0, y: 0.5, z: 0});
+        }
+    }
+
+    useEffect(() => {
+        const unsubscribeJump = subscribeKeys(
+            (state) => state.jump,
+            (value) => {
+                if (value)
+                    jump()
+            }
+        )
+
+        return () => {
+            unsubscribeJump();
+        }
+    }, [])
 
     useFrame((state, delta) => {
         const {forward, backward, leftward, rightward} = getKeys();
@@ -16,22 +43,22 @@ export default function Player() {
         const impulseStrength = 0.6 * delta;
         const torqueStrength = 0.2 * delta;
 
-        if(forward) {
+        if (forward) {
             impulse.z -= impulseStrength;
             torque.x -= torqueStrength;
         }
 
-        if(rightward) {
+        if (rightward) {
             impulse.x += impulseStrength;
             torque.z -= torqueStrength;
         }
 
-        if(backward) {
+        if (backward) {
             impulse.z += impulseStrength;
             torque.x += torqueStrength;
         }
 
-        if(leftward) {
+        if (leftward) {
             impulse.x -= impulseStrength;
             torque.z = torqueStrength;
         }
@@ -48,6 +75,8 @@ export default function Player() {
             colliders="ball"
             restitution={0.2}
             friction={1}
+            linearDamping={0.5}
+            angularDamping={0.5}
             position={[0, 1, 0]}
         >
             <mesh castShadow>
