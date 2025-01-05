7daf1343f960fd2d2524659486c3f9527e00c318
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-05 14:58:36 +0100

    handle reset when the ball falls

--- src/Player.js
+++ src/Player.js
@@ -14,7 +14,7 @@ export default function Player() {
     const end = useGame((state) => state.end);
     const blocksCount = useGame((state) => state.blocksCount);
 
-    const [smoothedCameraPosition] = useState(() => new THREE.Vector3(10,10,10));
+    const [smoothedCameraPosition] = useState(() => new THREE.Vector3(10, 10, 10));
     const [smoothedCameraTarget] = useState(() => new THREE.Vector3());
 
     const jump = () => {
@@ -24,32 +24,43 @@ export default function Player() {
         const ray = new rapier.Ray(origin, direction);
         const hit = world.castRay(ray, 10, true);
 
-        if(hit.timeOfImpact < 0.15) {
+        if (hit.timeOfImpact < 0.15) {
             body.current.applyImpulse({x: 0, y: 0.5, z: 0});
         }
     };
 
     const reset = () => {
-
+        body.current.setTranslation({x: 0, y: 1, z: 0})
+        body.current.setLinvel({x: 0, y: 0, z: 0})
+        body.current.setAngvel({x: 0, y: 0, z: 0})
     };
 
     useEffect(() => {
+        const unsubscribeReset = useGame.subscribe(
+            (state) => state.phase,
+            (phase) => {
+                if (phase === 'ready')
+                    reset();
+            }
+        )
+
         const unsubscribeJump = subscribeKeys(
             (state) => state.jump,
             (value) => {
                 if (value)
                     jump()
             }
-        )
+        );
 
         const unsubscribeAny = subscribeKeys(() => {
             start();
-        })
+        });
 
         return () => {
+            unsubscribeReset();
             unsubscribeJump();
             unsubscribeAny();
-        }
+        };
     }, []);
 
     useFrame((state, delta) => {
@@ -109,13 +120,11 @@ export default function Player() {
         /**
          * Phases
          */
-        if(bodyPosition.z < - (blocksCount * 4 + 2)) {
-            end();
-        }
+        if (bodyPosition.z < -(blocksCount * 4 + 2))
+            end()
 
-        if(bodyPosition.y > -4) {
-            restart();
-        }
+        if (bodyPosition.y < -4)
+            restart()
     });
 
     return (
--- src/stores/useGame.js
+++ src/stores/useGame.js
@@ -8,28 +8,34 @@ export default create(subscribeWithSelector((set) => {
          * Phases
          */
         phase: 'ready',
-        start: () => {
-            set((state) => {
-                if(state.phase === 'ready'){
-                    return {phase: 'playing'};
-                }
-                return {};
+        start: () =>
+        {
+            set((state) =>
+            {
+                if(state.phase === 'ready')
+                    return { phase: 'playing' }
+
+                return {}
             })
         },
-        restart: () => {
-            set((state) => {
-                if(state.phase === 'playing' || state.phase === 'ended'){
-                    return {phase: 'ready'};
-                }
-                return {};
+        restart: () =>
+        {
+            set((state) =>
+            {
+                if(state.phase === 'playing' || state.phase === 'ended')
+                    return { phase: 'ready' }
+
+                return {}
             })
         },
-        end: () => {
-            set((state) => {
-                if(state.phase === 'playing'){
-                    return {phase: 'ended'};
-                }
-                return {};
+        end: () =>
+        {
+            set((state) =>
+            {
+                if(state.phase === 'playing')
+                    return { phase: 'ended' }
+
+                return {}
             })
         },
     }
